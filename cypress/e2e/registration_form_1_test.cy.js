084e047b8ae89dbbb1a156149f69425dad7784f7
greensabre1 <daniiltodnem@gmail.com>
2023-09-12 01:54:32 +0300

    first commit

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -1,5 +1,5 @@
 // Before each test (it...) load .html page
-beforeEach(() => {
+beforeEach(() => {19-24 
     cy.visit('cypress/fixtures/registration_form_1.html')
 })
 
@@ -16,13 +16,18 @@ Assignment 2:
  5. Add comment to the first test containing today’s date
  */
 
-describe('This is first test suite', () => {
-    it('User can submit data only when valid mandatory values are added', () => {
-        cy.get('#username').type('Something')
-        cy.get('[data-testid="phoneNumberTestId"]').type('5656565656')
-        cy.get('input[name="password"]').type('Password123')
-        cy.get('[name="confirm"]').type('Password123')
+ // 8/27/2023
 
+
+ describe('This is first test suite, Daniil Todnem', () => {
+    it('User can submit data only when valid mandatory values are added', () => {
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
+        cy.get('#firstName').type('John')
+        cy.get('#lastName').type('Smith')       
+        cy.get('input[name="password"]').type('abcd')
+        cy.get('[name="confirm"]').type('abcd')
+        cy.get('#username').type('Somethsdfdfing')
+        
         //in order to activate submit button, user has to click somewhere outside the input field
         cy.get('h2').contains('Password').click()
         cy.get('.submit_button').should('be.enabled')
@@ -37,8 +42,7 @@ describe('This is first test suite', () => {
         // next 2 lines check exactly the same, but using different approach
         cy.get('#success_message').should('be.visible')
         cy.get('#success_message').should('have.css', 'display', 'block')
-    });
-
+        })
 
     it('User can use only same both first and validation passwords', () => {
         cy.get('#username').type('johnDoe')
@@ -62,14 +66,79 @@ describe('This is first test suite', () => {
     })
 
     it('User cannot submit data when username is absent', () => {
-        cy.get('#username').type('johnDoe')
-        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
-        cy.get("input[name='password']").type('Password123')
-        cy.get('[name="confirm"]').type('Password123')
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
+        cy.get('#firstName').type('John')
+        cy.get('#lastName').type('Smith')       
+        cy.get('input[name="password"]').type('abcd')
+        cy.get('[name="confirm"]').type('abcd')
+        cy.get('#username').type('Somethsdfdfing')
 
-        // Scroll back to username input field
+        // Scroll back to username input field and clear it
         cy.get('#username').scrollIntoView()
         cy.get('#username').clear()
+        cy.get('h2').contains('Password').click()
+
+        cy.get('.submit_button').should('be.disabled')
+        cy.get('#input_error_message').should('not.be.visible')
+    })
+})
+    /*
+    Assignment 3: add the content to the following tests
+    */
+
+describe('These are tests for assignment 3', () => {
+    it('User cannot submit data when phone number is absent', () => {
+        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+        cy.get('#firstName').type('John')
+        cy.get('#lastName').type('Smith')       
+        cy.get('input[name="password"]').type('abcd')
+        cy.get('[name="confirm"]').type('abcd')
+        cy.get('#username').type('Somethsdfdfing')
+        
+        // Clearing phone number and click
+        cy.get('[data-testid="phoneNumberTestId"]').clear()
+        cy.get('h2').contains('Password').click()
+
+        // Asserting that Submit button is disabled
+        cy.get('.submit_button').should('be.disabled')
+
+        // Assert that success message is not visible
+        cy.get('#success_message').should('not.be.visible')
+
+        // Assert that input error message is not visible
+        cy.get('#input_error_message').should('not.be.visible')
+    });
+    it('User cannot submit data when password and/or confirmation password is absent', () => {
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
+        cy.get('#firstName').type('John')
+        cy.get('#lastName').type('Smith')       
+        cy.get('input[name="password"]').type('abcd')
+        cy.get('#username').type('Somethsdfdfing')
+        cy.get('input[name="password"]').type('abcd')
+        cy.get('[name="confirm"]').type('abcd')
+       
+        // Clearing typed password confirmation and click
+        cy.get('[name="confirm"]').clear()
+        cy.get('h2').contains('Password').click()
+       
+        // Asserting that Submit button is disabled
+        cy.get('.submit_button').should('be.disabled')
+
+        // Assert that success message is not visible
+        cy.get('#success_message').should('not.be.visible')
+
+        // Assert that input error message is not visible
+        cy.get('#input_error_message').should('not.be.visible')
+    });
+    it('User cannot add letters to phone number', () => {
+        cy.get('[data-testid="phoneNumberTestId"]').type('Cerebrum Hub')
+        cy.get('#firstName').type('John')
+        cy.get('#lastName').type('Smith')       
+        cy.get('input[name="password"]').type('abcd')
+        cy.get('[name="confirm"]').type('abcd')
+        cy.get('#username').type('Somethsdfdfing')
+
+        // Click
         cy.get('h2').contains('Password').click()
 
         // Asserting that Submit button is disabled
@@ -79,40 +148,9 @@ describe('This is first test suite', () => {
         cy.get('#success_message').should('not.be.visible')
 
         // Assert that correct error message is visible and contain given text
-        cy.get('#input_error_message').should('be.visible').should('contain', 'Mandatory input field is not valid or empty!')
-
-        // Assert that username has tooltip with error message
-        cy.get('input[name="username"]').should('have.attr', 'title').should('contain', 'Input field')
-
-        // There are 2 options how to check error message visibility: using CSS or simply be.visible
-        // none = not visible; block = visible
-        cy.get('#input_error_message').should('be.visible')
-        cy.get('#input_error_message').should('have.css', 'display', 'block')
-    })
-
-    /*
-    Assignment 3: add the content to the following tests
-    */
-
-    it('User cannot submit data when phone number is absent', () => {
-        // Add test, similar to previous one with phone number field not filled in
-        // All other fields should be entered correctly
-        // Assert that submit button is not enabled and that successful message is not visible
-    })
-
-    it('User cannot submit data when password and/or confirmation password is absent', () => {
-        // Add test, similar to previous one with password field not filled in
-        // All other fields should be entered correctly
-        // Assert that submit button is not enabled and that successful message is not visible
-    })
-
-    it('User cannot add letters to phone number', () => {
-        // Next verification is given as example
-        // how we can check from html code, that phone number should contain only numbers
+        cy.get('#input_error_message').should('not.be.visible')
+        
+        // Check that phone number input is numbers
         cy.get('[data-testid="phoneNumberTestId"]').should('have.attr', 'type', 'number')
-
-        // Add steps, when all fields are correctly filled in, except phone number
-        // Try typing letters to phone number field
-        // Assert that submit button is not enabled and that successful message is not visible
-    })
-})
+    });
+});    
--- cypress/e2e/registration_form_3_test.cy.js
+++ cypress/e2e/registration_form_3_test.cy.js
@@ -2,26 +2,90 @@ beforeEach(() => {
     cy.visit('cypress/fixtures/registration_form_3.html')
 })
 
-/*
-BONUS TASK: add visual tests for registration form 3
-Task list:
-* Test suite for visual tests for registration form 3 is already created
-* Create tests to verify visual parts of the page:
-    * radio buttons and its content
-    * dropdown and dependencies between 2 dropdowns
-    * checkboxes, their content and links
-    * email format
- */
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
 
+const bday = '1994-07-15'
 
-/*
-BONUS TASK: add functional tests for registration form 3
-Task list:
-* Create second test suite for functional tests
-* Create tests to verify logic of the page:
-    * all fields are filled in + validation
-    * only mandatory fields are filled in + validations
-    * mandatory fields are absent + validations (try using function)
-    * If city is already chosen and country is updated, then city choice should be removed
-    * add file (google yourself for solution)
- */
+import { faker } from '@faker-js/faker'
+
+describe('Bonus Task 1: Visual Tests', () => {
+    it('Number of radio buttons is 4 and are clickable', () => {
+        cy.get('input[type="radio"]').should('have.length', 4)
+        cy.get('input[type="radio"]').eq(0).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(1).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(2).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(1).should('not.be.checked')
+        cy.get('input[type="radio"]').last().check().should('be.checked')
+        cy.get('input[type="radio"]').eq(2).should('not.be.checked')
+    })
+
+    it('Radio buttons have correct values', () => {
+        cy.get('input[type="radio"]').eq(0).should('have.attr', 'value', 'Daily')
+        cy.get('input[type="radio"]').eq(1).should('have.attr', 'value', 'Weekly')
+        cy.get('input[type="radio"]').eq(2).should('have.attr', 'value', 'Monthly')
+        cy.get('input[type="radio"]').eq(3).should('have.attr', 'value', 'Never')
+    })
+
+    it('Image logo exists and has correct size', () => {
+        cy.get('img[data-testid="picture"]').should('have.attr', 'src', 'cerebrum_hub_logo.png')
+            .and('is.visible')
+        cy.get('img[data-testid="picture"]').invoke('width').should('be.greaterThan', 170)
+            .and('not.be.lessThan', 150)
+        cy.get('img[data-testid="picture"]').invoke('height').should('be.greaterThan', 150)
+            .and('be.lessThan', 180)
+    })
+
+    it('Checkboxes can be checked and unchecked', () => {
+        cy.get('input[type="checkbox"]').should('have.length', 2)
+        cy.get('input[ng-model="checkbox"]').should('not.be.checked')
+        cy.get('input[ng-model="checkbox"]').next().should('not.be.checked')
+        cy.get('input[ng-model="checkbox"]').check().should('be.checked')
+            .uncheck().should('not.be.checked')
+        cy.get('input[ng-model="checkbox"]').next().check().should('be.checked')
+            .uncheck().should('not.be.checked')
+    })
+
+    it('Cookie policy link is clickable', () => {
+        cy.get('a').contains('Accept our cookie policy')
+            .should('have.attr', 'href', 'cookiePolicy.html')
+            .click()
+        cy.url().should('include', 'cookiePolicy.html')
+        cy.go('back').url().should('include', 'registration_form_3.html')
+    })
+})
+
+describe('Bonus Task 2: Functional Tests', () => {
+    it('Inputting all data and validating form', () => {
+        inputAllData()
+        cy.get('input[ng-disabled="myForm.$invalid"]').should('be.visible')
+            .click()
+        cy.get('h1').contains('Submission received').should('be.visible')
+    })
+
+    it('Inputting only mandatory data and form validation', () => {
+        inputMandatoryData()
+        cy.get('input[ng-disabled="myForm.$invalid"]').should('be.visible')
+            .click()
+        cy.get('h1').contains('Submission received').should('be.visible')
+    })
+})
+
+function inputAllData() {
+    cy.get('#name').clear().type(faker.person.firstName())
+    cy.get('input[name="email"]').type(faker.internet.email())
+    cy.get('label').contains('Date of birth').next().click().type(bday)
+    cy.get('input[type="radio"]').eq(0).check()
+    cy.get('#birthday').type(bday)
+    cy.get('input[ng-model="checkbox"]').check()
+    cy.get('input[ng-model="checkbox"]').next().check()
+    cy.get('#myFile').selectFile('cypress/fixtures/upload_file.html')
+}
+
+function inputMandatoryData() {
+    cy.get('#name').clear().type(faker.person.firstName())
+    cy.get('input[name="email"]').type(faker.internet.email())
+    cy.get('#birthday').type(bday)
+}
